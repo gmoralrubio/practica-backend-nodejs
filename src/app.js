88703614f1f8e80b0c2aedc1142869df8e4a4bc5
gmoralrubio <gmoralrubio@gmail.com>
2026-04-15 13:23:47 +0200

    feat: añadido favicon y configurados middlewares globales

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
-import express from 'express'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
+import express from 'express'
+import morgan from 'morgan'
+
+// Creamos la aplicación
 const app = express()
+const appDir = dirname(fileURLToPath(import.meta.url))
+
+// Global middlewares
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(join(appDir, '../public')))
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
 	res.send('Práctica final')
