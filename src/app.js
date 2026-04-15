b4670d09a353a977484857850b9ea2ad22ae200d
gmoralrubio <gmoralrubio@gmail.com>
2026-04-15 13:14:27 +0200

    feat: reestructurado servidor moviendo lógica a www

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
 import express from 'express'
 
 const app = express()
-const PORT = process.env.PORT
-const HOST = process.env.HOST
 
 app.get('/', (req, res) => {
 	res.send('Práctica final')
 })
 
-app.listen(PORT, HOST, () => {
-	console.log(`Server running on http://${HOST}:${PORT}`)
-})
+export default app
