c1e7d85b9c1a67ffad839a6c22cc3ac08f95b60a
gmoralrubio <gmoralrubio@gmail.com>
2026-04-15 16:00:04 +0200

    feat: añadida conexión a la base de datos con mongoose

---
+++ src/lib/database.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost27017'
+const DB_NAME = process.env.DB_NAME || 'demo'
+
+// Conexión a la BBDD
+export async function connectToDB() {
+	const mongooseInstance = await mongoose.connect(MONGODB_URI, {
+		dbName: DB_NAME,
+	})
+
+	console.log('Connected to MongoDB')
+
+	return mongooseInstance.connection
+}
