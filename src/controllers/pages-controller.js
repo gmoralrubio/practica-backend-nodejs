324024144c62b70002269b920d69e8d9edf1cec9
gmoralrubio <gmoralrubio@gmail.com>
2026-04-15 19:33:09 +0200

    feat: añadidos productos al controlador de la home

--- src/controllers/pages-controller.js
+++ src/controllers/pages-controller.js
@@ -1,5 +1,12 @@
-export function homePageController(req, res, next) {
+import { getProducts } from '../data/productRepository.js'
+
+export async function homePageController(req, res, next) {
+	const products = await getProducts()
+	console.log(products)
+
 	res.render('index.html', {
 		title: 'Home page',
+		products: products,
 	})
+	return
 }
