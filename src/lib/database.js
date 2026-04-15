24644a13d2428836ec518ba5bf655e74d471c5d7
gmoralrubio <gmoralrubio@gmail.com>
2026-04-15 19:33:35 +0200

    fix: corregida URI default de conexión a MongoDB

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost27017'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017'
 const DB_NAME = process.env.DB_NAME || 'demo'
 
 // Conexión a la BBDD
