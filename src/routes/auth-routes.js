0380535f129f0baecf3244758dc462347ab0839d
gmoralrubio <gmoralrubio@gmail.com>
2026-04-20 17:19:14 +0200

    feat: añadido logout

--- src/routes/auth-routes.js
+++ src/routes/auth-routes.js
@@ -2,9 +2,11 @@ import express from 'express'
 import {
 	loginPageController,
 	loginActionController,
+	logoutActionController,
 } from '../controllers/auth-controller.js'
 
 export const authRouter = express.Router()
 
 authRouter.get('/login', loginPageController)
 authRouter.post('/login', loginActionController)
+authRouter.get('/logout', logoutActionController)
