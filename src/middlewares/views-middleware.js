87954bdcc076eb1bc020f063d46daf7868d16908
gmoralrubio <gmoralrubio@gmail.com>
2026-04-29 19:20:15 +0200

    refactor: eliminado isModalOpen de res.locals

--- src/middlewares/views-middleware.js
+++ src/middlewares/views-middleware.js
@@ -3,6 +3,5 @@ import TAGS from '../models/TAGS.js'
 export function dataInViews(req, res, next) {
 	res.locals.errorMessage = null
 	res.locals.tags = TAGS
-	res.locals.isModalOpen = null
 	next()
 }
