788feabfad4657be588c4086116f359ea8a5a142
gmoralrubio <gmoralrubio@gmail.com>
2026-04-30 13:29:08 +0200

    style: eliminado código comentado en homePageController

--- src/controllers/pages-controller.js
+++ src/controllers/pages-controller.js
@@ -1,10 +1,6 @@
 import { getFilteredProducts, getProductsCount } from '../data/product-repository.js'
 
 export async function homePageController(req, res, next) {
-	// if (req.url !== '/') {
-	// 	req.productFilter.owner = req.session.userId
-	// }
-
 	const productsCount = await getProductsCount(req.productFilter)
 	const filteredProducts = await getFilteredProducts(
 		req.productQuery.skip,
